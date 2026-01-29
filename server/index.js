624c52256ac818bca1b1d0769e5b59a16f1c6d9a
Miyuki <7001vlbl@bk.ru>
2026-01-29 21:42:09 +0300

    подключил базу данных

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import { configDotenv } from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+const app = express()
+dotenv.config()
+
+// Constants
+const PORT = process.env.PORT || 3001
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+
+async function start () {
+    try {
+        await mongoose.connect (
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.h9j5vam.mongodb.net/${DB_NAME}?appName=Cluster0`,
+        )
+        
+        app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
